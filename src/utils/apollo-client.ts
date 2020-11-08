9d49cb4b021928c9e0607fe3db6f289512316e61
vitaliy khudonogov <3990608+quadgod@users.noreply.github.com>
2020-11-08 19:12:46 +0100

    remove token

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const apolloClient = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    authorization: 'Bearer 71e7e69158d622ead34ae45053b14d6458ed5c12',
+    authorization: 'Bearer',
   },
   cache: new InMemoryCache(),
 });
